81d6fcae2f94b4375a379ba0612c948798b89c23
mdsrmed <mdsrmed@gmail.com>
2023-09-02 16:39:09 -0400

    Initial Commit

---
+++ Customer/CustomerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomerApp.swift
+//  Customer
+//
+//  Created by Md Shohidur Rahman on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
