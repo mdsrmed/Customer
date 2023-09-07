cf72d45385a09f4ab9a111b3debeee0d7b36543c
mdsrmed <mdsrmed@gmail.com>
2023-09-07 18:19:39 -0400

    Launch screen and app icon

---
+++ Customer/Resources/Symbols.swift
@@ -0,0 +1,16 @@
+//
+//  Symbols.swift
+//  Customer
+//
+//  Created by Md Shohidur Rahman on 9/2/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
---
+++ Customer/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  Customer
+//
+//  Created by Md Shohidur Rahman on 9/2/23.
+//
+
+import Foundation
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let text = Color("text")
+    static let detailbackground = Color("detail-background")
+}
