6c5c713dccf0e6ea2c27ebd97087f1c13cf3f2a7
mdsrmed <mdsrmed@gmail.com>
2023-10-03 10:09:23 -0400

    Animated Pop-up view

---
+++ Customer/Base/Views/CheckmarkPopoverView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckmarkPopoverView.swift
+//  Customer
+//
+//  Created by Md Shohidur Rahman on 10/3/23.
+//
+
+import SwiftUI
+
+struct CheckmarkPopoverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckmarkPopoverView()
+}
